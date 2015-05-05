5fed469eb77ecb9f7a62f18b9b34c62434cc60cf
simonkirkham <simon.kirkham@gmail.com>
2015-05-05 20:41:00 +0100

    First Form fully working

--- src/DocumentGenerator/Controllers/HomeController.cs
+++ src/DocumentGenerator/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Novacode;
+using DocumentGenerator.Models;
 
 namespace DocumentGenerator.Controllers
 {
@@ -11,27 +8,58 @@ namespace DocumentGenerator.Controllers
     {
         public ActionResult Index()
         {
-            var document = DocX.Load(@"C:\Users\Si\Documents\GitHub\AnimalRecueDocumentGenereator\docs\2014 New Adoption form.doc");
+            return View();
+        }
 
-            document.ReplaceText("%POSTCODE%", "M219SP");
 
-            document.SaveAs("NewFile.docx");
+    }
+
+    public class DocumentController : Controller
+    {
+        [HttpGet]
+        public ActionResult AdoptionForm()
+        {
+            var model = new AdoptionFormModel
+            {
+                Date = DateTime.Now.Date
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AdoptionForm(AdoptionFormModel adoptionFormModel)
+        {
+            var document = Novacode.DocX.Load(@"C:\Users\Bob\Documents\Coding\DocumentGenereator\docs\2014 New Adoption form.docx");
+
+            document.ReplaceText("%DATE%", adoptionFormModel.Date.ToShortDateString());
+            document.ReplaceText("%NAME%", BuildAddress(adoptionFormModel.AddressLine1, adoptionFormModel.AddressLine2, adoptionFormModel.AddressLine3));
+            document.ReplaceText("%ADDRESS%", adoptionFormModel.Date.ToShortDateString());
+            document.ReplaceText("%POSTCODE%", adoptionFormModel.Postcode);
+            document.ReplaceText("%PHONENUMBER%", adoptionFormModel.TelephoneNumber);
+            document.ReplaceText("%EMAILADDRESS%", adoptionFormModel.EmailAddress);
+            document.ReplaceText("%ANIMALNAME%", adoptionFormModel.AnimalName);
+
+            document.SaveAs(String.Format(@"C:\Users\Bob\Documents\Coding\DocumentGenereator\docs\{0}.docx", adoptionFormModel.FullName));
 
             return View();
         }
 
-        public ActionResult About()
+        private string BuildAddress(string addressLine1, string addressLine2, string addressLine3)
         {
-            ViewBag.Message = "Your application description page.";
+            var address = addressLine1;
 
-            return View();
-        }
+            if (!string.IsNullOrWhiteSpace(addressLine2))
+            {
+                address += Environment.NewLine + addressLine2;
+            }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            if (!string.IsNullOrWhiteSpace(addressLine3))
+            {
+                address += Environment.NewLine + addressLine3;
+            }
 
-            return View();
+            return address;
         }
     }
 }
---
+++ src/DocumentGenerator/Models/AdoptionFormModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DocumentGenerator.Models
+{
+    public class AdoptionFormModel
+    {
+        [DisplayName("Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required]
+        public DateTime Date { get; set; }
+
+        [DisplayName("Name")]
+        [Required]
+        public string FullName { get; set; }
+
+        [DisplayName("Address Line 1")]
+        [Required]
+        public string AddressLine1 { get; set; }
+
+        [DisplayName("Address Line 2")]
+        public string AddressLine2 { get; set; }
+
+        [DisplayName("Address Line 3")]
+        public string AddressLine3 { get; set; }
+
+        [DisplayName("Postcode")]
+        [DataType(DataType.PostalCode)]
+        [Required]
+        public string Postcode { get; set; }
+
+        [DisplayName("Telephone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [Required]
+        public string TelephoneNumber { get; set; }
+
+        [DisplayName("Email Address")]
+        [EmailAddress]
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [DisplayName("Animal Name")]        
+        [Required]
+        public string AnimalName { get; set; }
+    }
+}
